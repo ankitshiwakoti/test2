0230cd2fe7c1bc19e85cdcb36a4b3bf8bcd80d84
ankit shiwakoti <shiwakotiankit@gmail.com>
2023-03-20 10:49:17 +0545

    3 col

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 import NFT from "./components/NFT";
 import Test from "./components/Test";
+import Test2 from "./components/Test2";
 
 function App() {
   return (
---
+++ src/components/Test2.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Test2 = () => {
+  return (
+    <div className="container">
+      <div class="d-flex flex-row bd-highlight mb-3">
+        <div class="p-2 bd-highlight">Flex item 1</div>
+        <div class="p-2 bd-highlight">Flex item 2</div>
+        <div class="p-2 bd-highlight">Flex item 3</div>
+      </div>
+    </div>
+  );
+};
+
+export default Test2;
