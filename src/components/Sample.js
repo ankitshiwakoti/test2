2298eb88509d10029bbdabf6608540e4ddf914e7
ankit shiwakoti <shiwakotiankit@gmail.com>
2023-03-21 07:26:01 +0545

    padding change

--- src/components/Sample.js
+++ src/components/Sample.js
@@ -8,10 +8,10 @@ const Sample = () => {
       <div className="row justify-content-start ">
         {new Array(6).fill("").map((items) => (
           <div class="col col-lg-4  col-sm-6 mt-5 ">
-            <div className=" text-start align-iteams-start mt-2 ">
+            <div className=" text-start  mt-2 ">
               <img src={pic1} alt="images ho " className="img-fluid mt-2" />
               <div class="text-color mt-4 ">Welcome</div>
-              <div class="card-body  mt-1 mr-5">
+              <div class="card-body ps-0 mt-1 ">
                 <p class="card-text text-color  ">
                   Lorem Ipsum is simply dummy text of the printing and
                   typesetting industry. Lorem Ipsum has
