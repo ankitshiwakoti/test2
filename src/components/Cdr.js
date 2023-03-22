f69c94fd423e9b8eec68796de1ee4876edf048e1
ankit shiwakoti <shiwakotiankit@gmail.com>
2023-03-22 08:07:27 +0545

    final edit

--- src/components/Cdr.js
+++ src/components/Cdr.js
@@ -19,27 +19,31 @@ const Cdr = () => {
   const html = "<div>Example HTML string</div>";
 
   return (
-    <div className="container-fluid">
-      <h2>Expert CDR Report Writing Service in Australia</h2>
-      <div className="row justify-content-center  text-align-left mt-4 ">
+    <div className="container">
+      <h2 className="mt-4">Expert CDR Report Writing Service in Australia</h2>
+      <div className="row justify-content-center  text-align-left mt-4  ">
         {post?.map((items) => (
           <>
             <div
-              className=" cdd col-lg-2 col-md-2 col-sm-4 mt-1 ms-1 "
-              style={{ width: "200px" }}
+              className="  cdd col-lg-2 col-md-2 col-sm-4 mt-1 ms-1 "
+              style={{ width: "150px" }}
               id={items.id}
             >
-              <img
-                src={items.image.data.attributes.url}
-                className="rounded-circle shadow-4-strong mt-2 "
-                alt="..."
-              />
+              <div className="circular--portrait">
+                <img
+                  src={items.image.data.attributes.url}
+                  className="rounded-circle shadow-4-strong mt-2 "
+                  alt="..."
+                />
+              </div>
             </div>
             <div className=" cdd col-lg-4 col-md-3 col-sm-6 mt-1  ">
               <div className="card-body">
-                <h5 className="card-title">{items.title}</h5>
+                <h5 className="card-title text-start">{items.title}</h5>
 
-                <p className="card-text mt-2">{parse(items.paragraph)}</p>
+                <p className="card-text mt-2 text-start">
+                  {parse(items.paragraph)}
+                </p>
               </div>
             </div>
           </>
