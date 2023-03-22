da53b6787651008bd1997a5dbb6b3e7599f7cce1
ankit shiwakoti <shiwakotiankit@gmail.com>
2023-03-22 15:22:12 +0545

    final

--- src/App.js
+++ src/App.js
@@ -51,23 +51,25 @@ function App() {
   const sample = [
     {
       title: "CDR Sample 1",
-      heading: "Simple things that matters a lot ",
+      heading: " Project name: Simple things that matters a lot ",
       paragraph:
-        "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.",
+        "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean com",
       id: 1,
     },
     {
       title: "CDR Sample 2",
-      heading: "unknown worls is filled with shit things that matters a lot ",
+      heading:
+        "Project name:unknown worls is filled with shit things that matters a lot ",
       paragraph:
-        "It is the first and simplest step in problem-solving. It organizes the steps/sequence required to formulate one goal out of multiple goals as well as actions to achieve that goal. Goal formulation is based on the current situation and the agent's performance measure (discussed below)",
+        "It is the first and simplest step in problem-solving. It organizes the steps/sequence required to formulate one goal out of multiple goals as well as actions to achieve that goal. Goal formulation is based on the current situation and the agent's performance measure (discussed below)  Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodos",
       id: 2,
     },
     {
       title: "CDR Sample 3",
-      heading: "Simple art is a kind of things that matters a lot ",
+      heading:
+        "Project name:Simple art is a kind of things that matters a lot ",
       paragraph:
-        "the problem implicitly. State-space of a problem is a set of all states which can be reached from the initial state followed by any sequence of actions. The state-space forms a directed map or graph where nodes are the states, links between the nodes are actions, and the path",
+        "the problem implicitly. State-space of a problem is a set of all states which the problem implicitly. State-space of a problem is a set of all states which can be reached from the initial state followed by any sequence of actions. The state-space forms a directed map or graph where nodes are the states, links between the nodes are actions, and the path",
       id: 3,
     },
   ];
--- src/components/Main.js
+++ src/components/Main.js
@@ -6,14 +6,14 @@ const Main = ({ data }) => {
   console.log(active);
   return (
     <div className="container">
-      <div className="row  mt-5">
+      <div className="row mt-5   ">
         {data?.map((items) => (
-          <div className="col-lg-4 p-0" key={items.id}>
+          <div className="col-lg-4  p-0" key={items.id}>
             <button
-              className={`${
+              className={`btn-lg w-100 border-0 rounded-0 ${
                 active === items?.title
-                  ? "text-white bg-secondary fw-bold underline btn-lg w-100 border-0"
-                  : "text-black  bg-light bg-gradient btn-lg w-100 border-0"
+                  ? "text-white bg-secondary fw-bold underline "
+                  : "text-black  bg-light bg-gradient "
               }`}
               onClick={() => setActive(items.title)}
             >
@@ -22,31 +22,60 @@ const Main = ({ data }) => {
           </div>
         ))}
       </div>
-      <div className="row bg-light bg-gradient  p-0 ">
-        <p className="text-start fs-5 mt-3">
-          {active === data[0]?.title && <p>{data[0].heading}</p>}
-          {active === data[1]?.title && <p>{data[1].heading}</p>}
-          {active === data[2]?.title && <p>{data[2].heading}</p>}
-        </p>
-        <p className="text-start fs-5">
-          {active === data[0]?.title && <p>{data[0].paragraph}</p>}
-          {active === data[1]?.title && <p>{data[1].paragraph}</p>}
-          {active === data[2]?.title && <p>{data[2].paragraph}</p>}
-        </p>
+      <div className="row bg-light bg-gradient mb-5  p-0 ">
+        {active === data[0]?.title && (
+          <p className="text-start ms-3 fs-6  mt-4">{data[0].heading}</p>
+        )}
+        {active === data[1]?.title && (
+          <p className="text-start ms-3 fs-6 mt-4"> {data[1].heading}</p>
+        )}
+        {active === data[2]?.title && (
+          <p className="text-start ms-3 fs-6 mt-4"> {data[2].heading}</p>
+        )}
+        {active === data[0]?.title && (
+          <p className="text-start ms-3 fs-6 "> {data[0].paragraph}</p>
+        )}
+        {active === data[1]?.title && (
+          <p className="text-start ms-3 fs-6 "> {data[1].paragraph}</p>
+        )}
+        {active === data[2]?.title && (
+          <p className="text-start ms-3 fs-6 "> {data[2].paragraph}</p>
+        )}
 
-        <div className=" p-0 bg-light bg-gradient ">
+        <div className=" p-0 bg-light bg-gradient  ">
           <ul class="list-group bg-light bg-gradient p-0 ms-4 ">
             <li class="list-group-item  d-flex  bg-light bg-gradient border-0">
-              <i class="bi bi-check-circle-fill me-1"> </i> A list item
+              <i class="bi bi-check-circle-fill me-3"> </i> State-space of a
+              problem is a set of all states which can be don.
             </li>
             <li class="list-group-item  d-flex  bg-light bg-gradient border-0">
-              <i class="bi bi-check-circle-fill me-1"> </i> A list item
+              <i class="bi bi-check-circle-fill me-3"> </i> A set of all states
+              which can be don
             </li>
             <li class="list-group-item  d-flex  bg-light bg-gradient border-0">
-              <i class="bi bi-check-circle-fill me-1"> </i> A list item
+              <i class="bi bi-check-circle-fill me-3"> </i> A list item nascetur
+              ridiculus mus. Donec quam felis
             </li>
             <li class="list-group-item  d-flex  bg-light bg-gradient border-0">
-              <i class="bi bi-check-circle-fill me-1"> </i> A list item
+              <i class="bi bi-check-circle-fill me-3"> </i> State-space of a
+              problem is a set of all.
+            </li>
+            <li class="list-group-item  d-flex  bg-light bg-gradient border-0">
+              <i class="bi bi-check-circle-fill me-3"> </i> A list item nascetur
+              ridiculus mus. Donec quam felis nascetur ridiculus mus. Donec quam
+              felis
+            </li>
+            <li class="list-group-item  d-flex  bg-light bg-gradient border-0">
+              <i class="bi bi-check-circle-fill me-3"> </i> State-space of a
+              problem is a set of all.
+            </li>
+            <li class="list-group-item  d-flex  bg-light bg-gradient border-0">
+              <i class="bi bi-check-circle-fill me-3"> </i> A list item nascetur
+              ridiculus mus. Donec
+            </li>
+            <li class="list-group-item  d-flex  bg-light bg-gradient border-0">
+              <i class="bi bi-check-circle-fill me-3"> </i> State-space of a
+              problem is a set of all.
             </li>
           </ul>
         </div>
